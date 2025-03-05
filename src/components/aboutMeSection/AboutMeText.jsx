c8a28dd780b65813ab3c8c511c1c6cd96ee756af
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-03-05 12:58:51 +0530

    Update AboutMeText.jsx

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-scroll";
-import YouTubeVideos from "./YoutubeVideos";
+import YoutubeVideos from "./YoutubeVideos";
 const AboutMeText = () => {
   return (
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
@@ -7,9 +7,9 @@ const AboutMeText = () => {
       <p className="text-darkGrey font-body">
         I’m a Senior Machine Learning Engineer. I make videos about Machine Learning, NLP, LLMs, Langchain,
         HuggingFace Models, BERT, Semantic Search, Elastic Search and Deep Learning.I have strong analytical 
-        ability to solve real world problems, a tech enthusiast and optimistic.
+        ability to solve real-world problems, a tech enthusiast and optimistic.
       </p>
-      <YouTubeVideos/>
+      <YoutubeVideos/>
       <a href="https://www.youtube.com/@AbidSaudagar">
       <button className="px-4 py-2 mr-3 rounded-full text-xl font-body text-white  border flex items-center gap-1  bg-black transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
        View More
